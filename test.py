c75cb2fa448e64863b6e5b480576f4de410496ac
abhishek bhingle <bhingleabhishek@gmail.com>
2020-07-05 20:35:44 +0530

    Initial commit

---
+++ test.py
@@ -0,0 +1,16 @@
+from collections import Counter
+for _ in range(int(input())):
+    n=int(input())
+    xco,yco=[],[]
+    for _ in range(4*n-1):
+        x,y=map(int,input().split())
+        xco.append(x)
+        yco.append(y)
+    xdic=Counter(xco)
+    ydic=Counter(yco)
+    for key, value in xdic.items():
+         if value %2!=0:
+             print(key,end=" ")
+    for key, value in ydic.items():
+         if value%2!=0:
+             print(key)
